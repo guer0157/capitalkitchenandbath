cec5030dbbe903246f8d96739a6c660ee920dd23
Cesar Guerrero <32390693+guer0157@users.noreply.github.com>
2026-01-19 16:53:12 -0500

    try this

--- js/form.js
+++ js/form.js
@@ -126,6 +126,7 @@ const setUpContactUsForm = () => {
     }
     elements.forEach((element) => {
       if (element[0].value.length === 0) {
+        console.log("Showing error for:", element[0].id);
         element[1].classList.add("d-block");
         hasErrors = true;
       } else {
